6fe938c196135b9aeefd893c620e2368849b551a
Oleksii <user@example.com>
2025-07-01 23:31:17 +0300

    1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import platform
 import aiohttp
 import asyncio
 
-from datetime import date
+from datetime import date, timedelta
 
 from urllib.parse import urlencode
 
@@ -19,34 +19,37 @@ def urls(number_of_days :int) -> list[str]:
     i = 0
     while i != number_of_days:
 
-        date_for_request = today.replace(day=today.day-1).strftime("%d.%m.%y")
+        date_for_request = (today - timedelta(days=i)).strftime("%d.%m.%Y")
         params = {"json": "", "date": date_for_request}
         url = f"https://api.privatbank.ua/p24api/exchange_rates?{urlencode(params)}"
+        i = i + 1
+        urls.append(url)
+    print(urls)
+    return urls
 
 
 
 
-async def main():
+async def main(list_of_url):
     
-    date = "01.07.2025"
-    params = {"json": "", "date": date}
-    url = f"https://api.privatbank.ua/p24api/exchange_rates?{urlencode(params)}"
-
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        result=[]
+        for url in list_of_url:
+            print(url)
+            async with session.get(url) as response:
 
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
-            print('Cookies: ', response.cookies)
-            print(response.ok)
-            result = await response.json()
-            return result
+                print("Status:", response.status)
+                print("Content-type:", response.headers['content-type'])
+                print('Cookies: ', response.cookies)
+                print(response.ok)
+                result_of_response = await response.json()
+                result.append(result_of_response)
+        return result
 
 
 if __name__ == "__main__":
-    today = date.today() #.strftime("%d.%m.%y")
-    print(today.replace(day=today.day+1).strftime("%d.%m.%y"))
+    
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main())
+    r = asyncio.run(main(urls(3)))
     print(r)
